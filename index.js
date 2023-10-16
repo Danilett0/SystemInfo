992bf5803433519b8c5c820e59cb72d3fc6015f6
Danilett0 <daniilo.97@hotmail.com>
2023-10-15 20:27:00 -0500

    implemento cors y nueva ruta para obtener la informacion

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.port || 3000;
 const os = require("node:os");
 
+app.use(cors());
+
 let systemInfo = {
-    cpuCount: os.cpus().length,
-    cpuDetails: os.cpus(),
-    osType: os.type(),
-    osArch: os.arch(),
-    osPlatform: os.platform(),
-    osRelease: os.release(),
-    hostname: os.hostname(),
-    uptime: os.uptime(),
-    totalMem: os.totalmem(),
-    freeMem: os.freemem(),
-    loadAvg: os.loadavg(),
-    homeDir: os.homedir(),
-    tmpDir: os.tmpdir(),
-    endianness: os.endianness(),
-    userInfo: os.userInfo(),
-    networkInterfaces: os.networkInterfaces()
+  cpuCount: os.cpus().length,
+  cpuDetails: os.cpus(),
+  osType: os.type(),
+  osArch: os.arch(),
+  osPlatform: os.platform(),
+  osRelease: os.release(),
+  hostname: os.hostname(),
+  uptime: os.uptime(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+  loadAvg: os.loadavg(),
+  homeDir: os.homedir(),
+  tmpDir: os.tmpdir(),
+  endianness: os.endianness(),
+  userInfo: os.userInfo(),
+  networkInterfaces: os.networkInterfaces(),
 };
 
 app.get("/", (req, res) => {
-  res.send(systemInfo);
-});
+    res.send("En esta app podras consultar la información mas relevante de tu Sistema Operativo");
+  });
+
+app.get("/systemInfo", (req, res) => {
+    res.status(200).send(systemInfo);
+  });
 
 app.listen(port, () => {
   console.log(`Aplicación escuchando en el puerto ${port}`);
